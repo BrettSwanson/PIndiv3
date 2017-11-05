e3be5909c7270293800c1d3c711ff78bbc213a85
BrettSwanson <swansb31@uwosh.edu>
2017-11-05 16:59:18 -0600

    added password reset

--- Q5.php
+++ Q5.php
@@ -11,16 +11,19 @@
       </code>
     </p>
   <form>
-    <div>
+    <div  class="question">
       <input type="radio" name="Q5">All of it will run with no error shown.
     </div>
-    <div>
+    <div  class="question">
       <input type="radio" name="Q5">An error will show, but the page will still
       try to run what it can.
     </div>
-    <div>
+    <div  class="question">
       <input type="radio" name="Q5"> Nothing will be shown past when an error
       occurs.
+    </div>
+    <div class="submit">
+      <input type="submit" value="Submit"/>
     </div>
   </form>
   <?php
