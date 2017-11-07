7d0617e3a3a45eaf81d93780808e87f95d634645
BrettSwanson <swansb31@uwosh.edu>
2017-11-07 10:44:41 -0600

    added example active question

---
+++ instructor/activated.php
@@ -0,0 +1,10 @@
+<?php
+  require("header.html");
+  require("nav.html");
+?>
+  <img src="Bar.jpg" alt="bar graph" />
+  <p>Correct Answer: A</p>
+  <a class="qDeactivate" href="questions.php">Deactivate</a>
+<?php
+  require("footer.html");
+?>
