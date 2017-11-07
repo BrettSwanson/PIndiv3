b5439c4f44604513e709d77a4c12d7134c8ff868
BrettSwanson <swansb31@uwosh.edu>
2017-11-07 11:11:00 -0600

    added statistics page

--- instructor/activated.php
+++ instructor/activated.php
@@ -4,6 +4,7 @@
 ?>
   <img src="Bar.jpg" alt="bar graph" />
   <p>Correct Answer: A</p>
+  <p>Time Left: 0:46</p>
   <a class="qDeactivate" href="questions.php">Deactivate</a>
 <?php
   require("footer.html");
---
+++ instructor/statistics.php
@@ -0,0 +1,37 @@
+<?php
+  require("header.html");
+  require("nav.html");
+?>
+  <div class="summary">
+    <p>Day 3</p>
+    <p>Average: 4 Points</p>
+    <hr/>
+    <p>Charlie: 10/10</p>
+    <p>Daniel: 2/10</p>
+    <p>David: 0/10</p>
+    <p>Sam: 4/10</p>
+    <p>Vince: 4/10</p>
+  </div>
+  <div class="summary">
+    <p>Day 2</p>
+    <p>Average: 4 Points</p>
+    <hr/>
+    <p>Charlie: 10/10</p>
+    <p>Daniel: 2/10</p>
+    <p>David: 0/10</p>
+    <p>Sam: 4/10</p>
+    <p>Vince: 4/10</p>
+  </div>
+  <div class="summary">
+    <p>Day 1</p>
+    <p>Average: 4 Points</p>
+    <hr/>
+    <p>Charlie: 10/10</p>
+    <p>Daniel: 2/10</p>
+    <p>David: 0/10</p>
+    <p>Sam: 4/10</p>
+    <p>Vince: 4/10</p>
+  </div>
+<?php
+  require("footer.html");
+?>
